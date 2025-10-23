c9127a8607b2f3ccfa63187b6ba32c7d0f22fca0
yutorb33 <ohtayuuto@gmail.com>
2025-10-23 11:09:14 +0900

    Made it handle arrays of different lengths and add openMP support

--- shannon_entropy_cpp/kullback_leibler_divergence.hpp
+++ shannon_entropy_cpp/kullback_leibler_divergence.hpp
@@ -4,6 +4,7 @@
 #include "adaptor.hpp"
 #include <cmath>
 #include <vector>
+#include <omp.h>
 
 
 using namespace nanoflann;
@@ -21,23 +22,27 @@ using kd_tree_t = nanoflann::KDTreeSingleIndexAdaptor<
 // Kullback-Leibler divergence
 // ============================================================
 template<typename T>
-T KL_div(T **X_ptr, T **Y_ptr, int k, int d, int N) {
-  if (N == 0) return 0.e0;
+T KL_div(T **X_ptr, T **Y_ptr, int k, int d, int N_X, int N_Y) {
+  if (N_X == 0 || N_Y == 0) return 0.e0;
   T *X = *X_ptr;
   T *Y = *Y_ptr;
+
   PointCloud cloud_X, cloud_Y;
-  cloud_X.N = N; cloud_X.dim = d; cloud_X.pts = X;
-  cloud_Y.N = N; cloud_Y.dim = d; cloud_Y.pts = Y;
+  cloud_X.N = N_X; cloud_X.dim = d; cloud_X.pts = X;
+  cloud_Y.N = N_Y; cloud_Y.dim = d; cloud_Y.pts = Y;
   kd_tree_t<T> index_X(d, cloud_X, KDTreeSingleIndexAdaptorParams(10));
   kd_tree_t<T> index_Y(d, cloud_Y, KDTreeSingleIndexAdaptorParams(10));
   index_X.buildIndex();
   index_Y.buildIndex();
+  T mean_log = 0.e0;
   // indices and distances
+  #pragma omp parallel reduction(+:mean_log)
+  {
   std::vector<size_t> ret_index(k+1);
   std::vector<T> out_dist_sqr(k+1);
-
-  T mean_log = 0.e0;
-  for (size_t i = 0; i < N; i++) {
+  
+  #pragma omp for schedule(static)
+  for (size_t i = 0; i < N_X; i++) {
     T *query_pt = &X[i*d];
     // X-tree (k+1)
     KNNResultSet<T> resultSet_X(k+1);
@@ -51,11 +56,13 @@ T KL_div(T **X_ptr, T **Y_ptr, int k, int d, int N) {
     index_Y.findNeighbors(resultSet_Y, query_pt, SearchParameters(0));
     //T s = std::sqrt(out_dist_sqr[k-1]);
     T s = out_dist_sqr[k-1];
+
     mean_log += std::log(s / r);
   }
-  mean_log /= N;
+  }
+  mean_log /= N_X;
   // Kullback-Leibler divergence
-  T Dkl = d * mean_log + std::log(T(N)/(N-1));
+  T Dkl = d * mean_log + std::log(T(N_Y)/(N_X-1));
   return Dkl > 0.e0 ? Dkl : 0.e0;
 }
 
