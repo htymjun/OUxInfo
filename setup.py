c257beefc4bc1b090db6d17f7a3f5cb715c2516c
htymjun <jhatayama@s.okayama-u.ac.jp>
2026-05-21 14:34:14 +0900

    Preparation for conda-forge

--- setup.py
+++ setup.py
@@ -64,15 +64,13 @@ setup(
   ext_modules=ext_modules,
   cmdclass={"build_ext": CustomBuildExt},
   zip_safe=False,
-  python_requires=">=3.12",
+  python_requires=">=3.10",
   install_requires=[
     'pybind11',
     'numpy',
     'matplotlib',
     'scipy',
-    'numba',
     'tqdm',
     'joblib',
   ],
 )
-
